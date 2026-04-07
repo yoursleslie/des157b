1fc68abbe28da7cb3fff8e7e04eccf85ea2414bb
Leslie Navarro <lesnavarro@gmail.com>
2026-04-07 12:09:57 -0700

    portal finish

--- script.js
+++ script.js
@@ -3,27 +3,33 @@
 
     const button = document.querySelector('button');
     const body = document.querySelector('body');
-    const banner = document.querySelector('#banner');
-    const sections = document.querySelectorAll('section')
-    let mode = 'dark';
+    const sections = document.querySelectorAll('section');
+    const imgLight = document.querySelector('#img-light');
+    const imgDark = document.querySelector('#img-dark');
+    const switchImg = document.querySelector('#switch');
+    let mode = 'light';
 
     button.addEventListener('click', function() {
-        if (mode === 'dark') {
+        if (mode === 'light') {
+            imgLight.style.display = 'none';
+            imgDark.classList.add('visible');
+            switchImg.src = 'images/yoda.png';
+            switchImg.alt = 'yoda';
             body.className = 'switch';
-            banner.className = 'switch';
-            button.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
             }
-            mode = 'light';
+            mode = 'dark';
         } else {
+            imgLight.style.display = 'block';
+            imgDark.classList.remove('visible');
+            switchImg.src = 'images/maul.png';
+            switchImg.alt = 'darth maul';
             body.removeAttribute('class');
-            banner.removeAttribute('class');
-            button.removeAttribute('class');
             for (const section of sections) {
                 section.removeAttribute('class');
             }
-            mode = 'dark'
+            mode = 'light';
         }
-    })
-})()
+    });
+})();
